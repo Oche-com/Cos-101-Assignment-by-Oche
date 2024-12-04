ebba3676a730ed42cd9624e8b310921ba4b67f15
Oche-com <oche89140@gmail.com>
2024-12-04 22:32:54 +0100

    COC101 Optional Assignment.py

---
+++ Optional Assignment.py
@@ -0,0 +1,9 @@
+def sort_ascending(numbers):
+    numbers.sort()
+
+numbers = [9, 1, 7, 2, 6, 3, 8, 4, 5]
+sort_ascending(numbers)
+
+print("Sorted numbers in ascending order:")
+for number in numbers:
+    print(number)
